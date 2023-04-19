dd3c7ab5f78e3365d153622ab8fa9e20bc84efb4
fredrick amoako <fredrickamoako1@gmail.com>
2023-04-19 23:23:59 +0000

    Add project files.

---
+++ SimpleGC/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGC
+{
+    internal class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+        public Car()
+        {
+            
+        }
+        public Car(string name, int speed)
+        {
+            PetName = name;
+            CurrentSpeed = speed;
+        }
+
+        public override string ToString()
+        {
+            return $"{PetName} is going {CurrentSpeed} MPH";
+        }
+    }
+}
